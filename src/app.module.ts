9bf1202ba3f6b2617eeadc1bfb01aa311cc10584
ulock <duongbui2612@gmail.com>
2024-09-12 15:21:26 +0700

    3000/graphql to go to graphql playground

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule,
+ 
+    GraphQLModule.forRoot<ApolloDriverConfig>
+    (
+      {
+        driver: ApolloDriver,
+        autoSchemaFile: 'src/schema.gql',
+        playground: true,
+      }
+    )
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,20 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
+
 
 @Module({
   providers: [UserResolver, UserService],
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>
-    (
-      {
-        driver: ApolloDriver,
-        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      }
-    )
-  ],
+  
 })
 export class UserModule {}
