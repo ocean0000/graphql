eca7d75f20b7d3142a5d802ed6e9df2470edc366
ocean <duongbui2612@gmail.com>
2024-09-27 20:21:12 +0700

    nothing

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { UpdateUserInput } from './dto/update-user.input';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Users } from './database/users.schema';
+import { UsersModule } from './users.module';
 
 
 
@@ -11,7 +12,6 @@ import { Users } from './database/users.schema';
 export class UsersService {
 
   @InjectModel(Users.name) private readonly userModel: Model<Users>
-
   create(createUserInput: CreateUserInput) {
     
     return this.userModel.create(createUserInput)
